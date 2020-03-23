c9331f4b73f19c5af808fad1c61f3b59e82d03bc
rabiarsl <rabiarslan7@gmail.com>
2020-03-23 20:44:15 +0300

    Flask inserted for remote mechanism

--- src/BottleDetection.py
+++ src/BottleDetection.py
@@ -1,8 +1,23 @@
+from builtins import print
+from flask import Response
+import jsonpickle
+import cv2
+from future.types.newrange import range_iterator
 from imageai.Detection import ObjectDetection
 import os
 import pickle
+import ast
 from matplotlib import image
 import numpy as np
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.secret_key = "secret key"
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 filename = 'model_glass_vs_tin.sav'
 glass_vs_tin_model = pickle.load(open(filename, 'rb'))
@@ -17,9 +32,9 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath(os.path.join(execution_path, "../dataset/resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
 
-
 def bottle_detection(path):
-    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, path), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, path),
+                                                 output_image_path=os.path.join(execution_path, "imagenew.jpg"))
     for eachObject in detections:
         if "bottle" == eachObject["name"]:
             if eachObject["percentage_probability"] > 50:
@@ -49,4 +64,64 @@ def bottle_type(path, bottle_type):
     return False
 
 
-print(bottle_type("deneme.jpg", "plastic"))
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# route http posts to this method
+@app.route('/plastic', methods=['POST'])
+def plastic():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    im = cv2.imread("image.jpg", cv2.IMREAD_UNCHANGED)
+    result = bottle_type("image.jpg", "plastic")
+    # build a response dict to send back to client
+    response = {'message': result
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+# route http posts to this method
+@app.route('/glass', methods=['POST'])
+def glass():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    result = bottle_type("image.jpg", "glass")
+    # build a response dict to send back to client
+    response = {'message': result
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+# route http posts to this method
+@app.route('/tin', methods=['POST'])
+def tin():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite("image.jpg", img)
+    result = bottle_type("image.jpg", "tin")
+    # build a response dict to send back to client
+    response = {'message': result
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.16', threaded=False)
---
+++ src/SendImage.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import requests
+import json
+import cv2
+
+addr = 'http://192.168.1.16:5000'
+test_url = addr + '/plastic'
+
+# prepare headers for http request
+content_type = 'image/jpeg'
+headers = {'content-type': content_type}
+img = cv2.imread('deneme.jpg')
+# encode image as jpeg
+_, img_encoded = cv2.imencode('.jpg', img)
+# send http request with image and receive response
+response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+# decode response
+print(json.loads(response.text))
